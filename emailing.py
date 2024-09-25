520c0b6dd2d02f0217d514915f6fa8487a51ffb6
stephenmwise <162519976+stephenmwise@users.noreply.github.com>
2024-09-24 21:49:08 -0400

    Added threading and emailing

--- emailing.py
+++ emailing.py
@@ -1,2 +1,20 @@
-def send_email():
-    print("Email was sent!")
+import smtplib
+from email.message import EmailMessage
+
+PASSWORD = "Password123"
+SENDER = test@gmail.com
+
+
+def send_email(image_path):
+    email_message = EmailMessage()
+    email_message["Subject"] = "Alert: Motion Detected"
+    email_message.set_content("A person has entered your room.")
+
+    with open(image_path, "rb") as file:
+        content = file.read()
+    email_message.add_attachment(content, maintype="image", subtype=imghdr.what(None, content))
+
+    gmail = smtplib.SMTP("smtp.gmail.com", 587)
+    gmail.ehlo()
+    gmail.starttls()
+    gmail.login(SENDER, PASSWORD)
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import cv2
 import time
+import glob
 from emailing import send_email
+from threading import Thread
 
 # Select video source as main camera
 video = cv2.VideoCapture(0)
@@ -8,6 +10,13 @@ time.sleep(1)
 
 first_frame = None
 status_list = []
+count = 1
+
+def clean_folder():
+    images = glob.glob("images/*.png")
+    for image in images:
+        os.remove(image)
+
 while True:
     status = 0
     # Get Video
@@ -37,12 +46,25 @@ while True:
         rectangle = cv2.rectangle(frame, (x,y), (x+w, y+h), (0, 255, 0), 3)
         if rectangle.any():
             status = 1
+            # Create image files of frames
+            cv2.imwrite(f"images/{count}.png", frame)
+            count += 1
+            all_images = glob.glob("images/*.png")
+            index = int(len(all_images) / 2)
+            image_with_object = all_images[index]
 
+    # Status of two most recent frames with or without object in video frame
     status_list.append(status)
     status_list = status_list[-2:]
 
     if status_list[0] == 1 and status_list[1] == 0:
-        send_email()
+        email_thread = Thread(target=send_email, args=(image_with_object, ))
+        email_thread.daemon = True
+        clean_thread = Thread(target=clean_folder)
+        email_thread.daemon = True
+
+        email_thread.start()
+
     print(status_list)
 
     cv2.imshow("Webcam Monitor Video", frame)
@@ -53,3 +75,4 @@ while True:
         break
 
 video.release()
+clean_thread.start()
